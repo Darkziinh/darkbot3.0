04caa90bd6d35b7f619a96cfaee26b130af2caac
Darkziinh <76751583+Darkziinh@users.noreply.github.com>
2021-02-06 15:43:09 -0300

    Create suporte.java

---
+++ Linux/suporte.java
@@ -0,0 +1,13 @@
+class Sample extends Object {
+    int ivar1;
+    int ivar2;
+
+    Sample(int i, int j) {
+        ivar1 = i;
+        ivar2 = j;
+    }
+
+    int emptyMethod() {}
+
+    ... = Linux
+}
