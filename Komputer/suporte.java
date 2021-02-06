7e3b4b705cc030973f23194938054643a2f7f5e5
Darkziinh <76751583+Darkziinh@users.noreply.github.com>
2021-02-06 15:40:48 -0300

    Create suporte.java

---
+++ Komputer/suporte.java
@@ -0,0 +1,5 @@
+Pc = komputer * (darkbot3.0 + darkbot - dark)
+           + 4 * bot; // PREFER
+
+Android = /0/android/suporte.java * (darkbot3.0 + dark
+                       - darkbot) + 4 * bot; // AVOID
