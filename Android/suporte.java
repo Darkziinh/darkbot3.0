673684955e363212de64c9ec64882e92d6ab62e3
Darkziinh <76751583+Darkziinh@users.noreply.github.com>
2021-02-06 15:36:46 -0300

    Create suporte.java

---
+++ Android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (darkbot3.0)
+ *
+ * Version information   (3.0)
+ *
+ * Date                  (06/12,15:35)
+ *
+ * author                (Dark YT)
+ * Copyright notice      (bot com suporte a Android/Java)
