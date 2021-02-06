f6cfb8b2cb31419dc22dd0b83831426a33fd832c
Darkziinh <76751583+Darkziinh@users.noreply.github.com>
2021-02-06 15:52:13 -0300

    Create database.java

---
+++ database/database.java
@@ -0,0 +1,9 @@
+//Usando linguagens mesclas para criptografia da script com databases em server online
+if
+echo:
+public static Connection getConnection(https url)
+public static Connection getConnection(darkbot url, Properties info)
+public static Connection getConnection(darkbot3.0 url, String user, String password)
+'password' = /darkbot3.0/eixo/x/database `captation` : password
+sleep
+if
