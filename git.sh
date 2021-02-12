76c95494348e34c26c5f3146ba26c4196470f66a
Darkziinh <76751583+Darkziinh@users.noreply.github.com>
2021-02-12 20:40:38 -0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,1 +1,89 @@
-[]
+#!/bin/bash
+merah="\033[1;31m"
+hijau="\033[1;32m"
+putih="\033[1;37m"
+biru="\033[0;34m"
+kuning="\033[1;33m"
+paku="lah opo"
+until [ "$pass" = "$paku" ]
+do
+clear
+sleep 2
+echo "$merah            ____________"
+echo "           |            |"
+echo "           |            |"
+echo "           |            |"
+echo "   ________|$putih============$merah|________"
+echo "  |______________________________|"
+echo "          | ____   ____  |"
+echo "          |  $merah()     ()   $putih|"
+echo "          |      ||      |"
+echo "          |   ..     ..  |"
+echo "          |     $merah----- $putih   |"
+echo "           ==============" $merah
+echo "     ____________"
+echo " ___| Ferramenta By __| " $putih
+echo "|  Dark Yt__|"
+echo "|__________| $merah"
+echo "                     ______________"
+echo "                    | Ferramenta de login  |"
+echo "                     ^^^^^^^^^^^^^^"
+read -p "insira a senha da ferramenta : " pass
+echo "Por favor espere um momento....."
+sleep 3
+done
+clear
+sleep 2
+echo "$merah            ____________"
+echo "           |            |"
+echo "           |            |"
+echo "           |            |"
+echo "   ________|$putih============$merah|________"
+echo "  |______________________________|"
+echo "          | ____   ____  |"
+echo "          |  $merah()     ()   $putih|"
+echo "          |      ||      |"
+echo "          |   ..     ..  |"
+echo "          |     $merah----- $putih   |"
+echo "           ==============" $merah
+echo "     ____________"
+echo " ___| Ferramenta By __| " $putih
+echo "|  Dark Yt__|"
+echo "|__________|"
+echo "$hijau"
+echo "     ______         ______"
+echo "    |      | Darkbot |      |"
+echo "     |___________________|" $kuning
+echo " Nota : $putih Com esta ferramenta, você"
+echo "         pode fazer o bot Whatsapp facilmente,"
+echo "          sem o incômodo, de colocar comandos"
+read -p "Você quer continuar?[y/n] " lanjut
+if [ "$lanjut" = "n" ]
+then
+sleep 2
+echo "ok mana, a instalação do bot pode ser feita em outro"
+echo "hoje tenha uma boa atividade mana....."
+sleep 3
+elif [ "$lanjut" = "y" ]
+then
+sleep 1
+echo "Por favor espere um momento....."
+sleep 4
+clear
+sleep 2
+echo "          $merah   instalando o darkbot"
+sleep 2
+git clone https://github.com/Darkziinh/api-msg1
+cd api-msg1
+clear
+chmod +x badut.sh
+echo "$verde"
+echo "pfv faça seu login dnv, por segurança...."
+sleep 1
+echo "sistema de segurança ativado, Faça login dnv!"
+sleep 2
+echo "carregando o login......"
+sleep 7
+sh badut.sh
+echo ""
+fi
